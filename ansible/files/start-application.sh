d34b453c889c5d09e45a0642f4138af0f3916fe7
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2020-07-22 17:30:34 +0200

    Pull artifacts for application 'start-application.sh'; New env vars

--- ansible/files/start-application.sh
+++ ansible/files/start-application.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
-while getopts ":r:c:" opt; do
-  case $opt in
-  r)
-    REGISTRY_URL="$OPTARG"
-    ;;
-  \?)
-    echo "Invalid option -$OPTARG" >&2
-    ;;
-  esac
-done
+# Load Docker Compose files for application from project's bucket
+s3cmd get "$COMPOSE_FILES_BUCKET_PATH" /usr/local/app/ --recursive
 
 # Log into AWS ECR
-aws ecr get-login-password | docker login -u AWS --password-stdin $REGISTRY_URL
+aws ecr get-login-password | docker login -u AWS --password-stdin "$ECR_BASE_URL"
 if [ $? -ne 0 ]; then
   echo 'Docker failed to log into ECR. Exiting...'
   exit 1
 fi
 
-# Pull Docker images for service
+# Pull Docker images for application
 docker-compose -f /usr/local/app/docker-compose.yml -f /usr/local/app/docker-compose.prod.yml pull
 if [ $? -ne 0 ]; then
   echo 'Failed to pull images. Exiting...'
   exit 1
 fi
 
-# Run docker-compose to run service
+# Run docker-compose to run application
 docker-compose -f /usr/local/app/docker-compose.yml -f /usr/local/app/docker-compose.prod.yml up -d
 if [ $? -ne 0 ]; then
   echo 'Failed to bring up application with docker-compose. Exiting...'
