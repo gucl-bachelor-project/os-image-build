4f61324a50cde8aaa70bc7bb1e246aa8c3534b0d
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2020-07-22 22:03:10 +0200

    More verbose logs in 'start-application.sh'; sudo command for s3cmd

--- ansible/files/start-application.sh
+++ ansible/files/start-application.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# Load Docker Compose files for application from project's bucket
-s3cmd get "$COMPOSE_FILES_BUCKET_PATH" /usr/local/app/ --recursive
+echo 'Fetching Docker Compose files for application from project'\''s bucket...'
+sudo s3cmd get "$COMPOSE_FILES_BUCKET_PATH" /usr/local/app/ --recursive
+if [ $? -ne 0 ]; then
+  echo 'Failed to fetch files. Exiting...'
+  exit 1
+fi
 
-# Log into AWS ECR
+echo 'Logging into AWS ECR...'
 aws ecr get-login-password | docker login -u AWS --password-stdin "$ECR_BASE_URL"
 if [ $? -ne 0 ]; then
   echo 'Docker failed to log into ECR. Exiting...'
   exit 1
 fi
 
-# Pull Docker images for application
+echo 'Pulling Docker images for application...'
 docker-compose -f /usr/local/app/docker-compose.yml -f /usr/local/app/docker-compose.prod.yml pull
 if [ $? -ne 0 ]; then
   echo 'Failed to pull images. Exiting...'
@@ -17,6 +21,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Run docker-compose to run application
+echo 'Starting Docker Compose setup for application...'
 docker-compose -f /usr/local/app/docker-compose.yml -f /usr/local/app/docker-compose.prod.yml up -d
 if [ $? -ne 0 ]; then
   echo 'Failed to bring up application with docker-compose. Exiting...'
